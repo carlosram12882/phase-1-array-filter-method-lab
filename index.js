37e15579e28fa708335e1452d67e944a61b80fef
Carlos Ramirez <carlosramirez686@gmail.com>
2022-01-09 09:56:37 -0800

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution here
+function findMatching(arr, driver){
+    const result = arr.filter(function(drive){
+    return driver.toLowerCase() === drive.toLowerCase()
+    });
+   return result
+}
+
+function fuzzyMatch(arr, driver){
+    const result = arr.filter(function(drive){
+        return driver.substr(0,2) === drive.substr(0,2)
+    });
+    return result
+}
+
+function matchName (arr, driver){
+    console.log(arr, driver)
+    return arr.filter(function(drive){
+        console.log(drive)
+    return    drive.name === driver
+        
+        
+            //    newArr = []
+        //    newArr.push(drive.name + drive.hometown)
+        //    return newArr
+        //}
+
+    });
+    
+}
+//const result = words.filter(word => word.length > 6);
